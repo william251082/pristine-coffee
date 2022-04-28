0a5443024b06086c2fd8c78bae818c731d904b38
william <william@magicmanager.nl>
2022-04-28 18:11:40 +0200

    Invoke Unsplash API for Coffee Store Images

--- lib/coffeeStores.ts
+++ lib/coffeeStores.ts
@@ -1,4 +1,8 @@
 import {fetchStoreById, fetchStores} from "@lib/db";
+import { createApi } from "unsplash-js";
+
+const accessKey = typeof process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY !== 'undefined' ? process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY : ''
+const unsplashApi = createApi({accessKey})
 
 export const getCoffeeStores = async () => {
     return await fetchStores('52.3676,4.9041', '6', 'coffee')
