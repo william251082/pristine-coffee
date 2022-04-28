d7e61bf39b5a73373ede53baeac9bee789c5ddb3
william <william@magicmanager.nl>
2022-04-28 12:47:50 +0200

    Card Component Styling

--- components/core/Card/Card.tsx
+++ components/core/Card/Card.tsx
@@ -3,18 +3,26 @@ import styles from "./Card.module.css";
 import {CardProps} from "@components/core/Card/CardTypes";
 import Image from "next/image";
 import Link from "next/link";
+import cn from "classnames";
 
 const Card: FC<CardProps> = ({name, imgUrl, href}) => {
     return (
         <Link href={href}>
-            <a>
-                <h2>{name}</h2>
-                <Image
-                    src={imgUrl}
-                    width={260}
-                    height={160}
-                    alt="card image"
-                />
+            <a className={styles.cardLink}>
+                <div className={cn("glass", styles.container)}>
+                    <div className={styles.cardHeaderWrapper}>
+                        <h2 className={styles.cardHeader}>{name}</h2>
+                    </div>
+                    <div className={styles.cardImageWrapper}>
+                        <Image
+                            className={styles.cardImage}
+                            src={imgUrl}
+                            width={260}
+                            height={160}
+                            alt="card image"
+                        />
+                    </div>
+                </div>
             </a>
         </Link>
     )
