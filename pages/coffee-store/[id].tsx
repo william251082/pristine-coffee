e42ce0bca4bc2dee5128d297924f670a39759c27
william <william@magicmanager.nl>
2022-04-28 13:59:13 +0200

    Coffee Stores Heading

--- pages/coffee-store/[id].tsx
+++ pages/coffee-store/[id].tsx
@@ -1,14 +1,39 @@
 import {useRouter} from "next/router"
 import styles from "@styles/CoffeeStore.module.css"
 import Link from "next/link";
+import {CoffeeStore, coffeeStores} from "@data/coffeeStores";
+import {GetStaticPropsContext} from "next";
+import {FC, JSXElementConstructor, ReactElement, ReactFragment, ReactPortal} from "react";
 
-const CoffeeStore = () => {
+export async function getStaticProps({params}: GetStaticPropsContext) {
+    return {
+        props: {
+            coffeeStore: coffeeStores.find((coffeeStore: CoffeeStore) => {
+                return coffeeStore.id.toString() === params?.id
+            })
+        }
+    }
+}
+
+export async function getStaticPaths() {
+    return {
+        paths: [
+            {params: {id: '0'}},
+            {params: {id: '1'}}
+        ],
+        fallback: true
+    }
+}
+
+const CoffeeStore = (props: { coffeeStore: CoffeeStore}) => {
     const router = useRouter()
     return (
         <div className={styles.layout}>
             Coffee Store Page {router.query.id}
             <Link href="/"><a>Back to home</a></Link>
             <Link href="/coffee-store/dynamic"><a>Go to Page</a></Link>
+            <p>{props.coffeeStore.address}</p>
+            <p>{props.coffeeStore.name}</p>
         </div>
     );
 };
