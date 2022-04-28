39a9951514e7f6af3dd2aa4e9a5d7a6a07955ec4
william <william@magicmanager.nl>
2022-04-28 14:36:26 +0200

    Coffee Store Page Styling

--- pages/coffee-store/[id].tsx
+++ pages/coffee-store/[id].tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import {CoffeeStore, coffeeStores} from "@data/coffeeStores";
 import {GetStaticPropsContext} from "next";
 import Head from "next/head";
+import Image from "next/image"
+import cn from "classnames";
 
 interface CoffeeStoreProps {
     coffeeStore: CoffeeStore
@@ -31,19 +33,45 @@ const CoffeeStore = ({coffeeStore}: CoffeeStoreProps) => {
     if (router.isFallback) {
         return <div>Loading...</div>
     }
-    const {address, name, neighbourhood} = coffeeStore
+    const {address, name, imgUrl} = coffeeStore
     return (
         <div className={styles.layout}>
             <Head>
                 <title>{name}</title>
                 <meta name="description" content={`${name} coffee store`} />
             </Head>
-            Coffee Store Page {router.query.id}
-            <Link href="/"><a>Back to home</a></Link>
-            <Link href="/coffee-store/dynamic"><a>Go to Page</a></Link>
-            <p>{address}</p>
-            <p>{name}</p>
-            <p>{neighbourhood}</p>
+            <div className={styles.container}>
+                <div className={styles.col1}>
+                    <div className={styles.backToHomeLink}>
+                        <Link href="/"><a>← Back to home</a></Link>
+                    </div>
+                    <div className={styles.nameWrapper}>
+                        <h1 className={styles.name}>{name}</h1>
+                    </div>
+                    <Image
+                        src={
+                            imgUrl ||
+                            "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+                        }
+                        width={600}
+                        height={360}
+                        className={styles.storeImg}
+                        alt={name}
+                    />
+                </div>
+                <div className={cn("glass", styles.col2)}>
+                    <div className={styles.iconWrapper}>
+                        <Image
+                            src="/static/icons/places.svg"
+                            width="24"
+                            height="24"
+                            alt="places icon"
+                        />
+                        <p className={styles.text}>{address}</p>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
