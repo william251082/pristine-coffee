941c745bbe507db893fd47d510eb05f7347c80ce
william <william@magicmanager.nl>
2022-04-29 08:52:05 +0200

    Find Coffee Store API

--- pages/api/createCoffeeStore.ts
+++ pages/api/createCoffeeStore.ts
@@ -4,7 +4,7 @@ import {table} from "@lib/airtable";
 
 const createCoffeeStore = async (req:NextApiRequest, res: NextApiResponse<CreateCoffeeStoreResponse>) => {
     if (req.method === "POST") {
-        const {id, name, neighbourhood, address, imgUrl, voting} = req.body;
+        const {id, name, neighbourhood, address, imgUrl, voting} = req.body
         try {
             const findCoffeeStoreRecords = await table.select({
                 filterByFormula: 'id="0"'
@@ -15,11 +15,11 @@ const createCoffeeStore = async (req:NextApiRequest, res: NextApiResponse<Create
                 //create a record
             }
         } catch (err) {
-            console.error("Error creating or finding a store", err);
-            res.status(500);
-            res.json({message: "Error creating or finding a store", err});
+            console.error("Error creating or finding a store", err)
+            res.status(500)
+            res.json({message: "Error creating or finding a store", err})
         }
     }
-};
+}
 
 export default createCoffeeStore;
